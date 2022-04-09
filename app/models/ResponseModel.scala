ae530b2e6cdaef1b3edcbdf44fcfa6bf9f5a6568
Jow Pavan <jonathan@jrpavan.com>
2022-04-09 18:43:48 +0100

    Fix response

--- app/models/ResponseModel.scala
+++ app/models/ResponseModel.scala
@@ -2,5 +2,5 @@ package models
 
 case class ResponseBase(message: String)
 case class RecipeListResponse(recipes: Seq[Recipe])
-case class RecipeResponse(message: String, recipes: Seq[Recipe])
+case class RecipeResponse(message: String, recipe: Seq[Recipe])
 case class CreateErrorResponse(message: String, required: String)
